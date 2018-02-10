edd9d5a6c0eacb597acffe643042e3071664ad34
Yevhenii_Yurkov <yevheniiyurkov@gmail.com>
2018-02-10 21:45:06 +0200

    Intial angular adding

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,20 @@
-//module ua.yurkov.amazingchatting {
-//    //jpa
-//    requires hibernate.jpa;
-//
-//    //spring
-//    requires spring.beans;
-//    requires spring.core;
-//    requires spring.context;
-//    requires spring.tx;
-//    requires spring.web;
-//    requires spring.data.commons;
-//    requires spring.data.jpa;
-//    requires spring.boot;
-//    requires spring.boot.autoconfigure;
-//
-//    //logging
-//    requires slf4j.api;
-//
-//    //mapper
-//    requires modelmapper;
-//}
+module ua.yurkov.amazingchatting {
+    //jpa
+    requires hibernate.jpa;
+
+    //spring
+    requires spring.context;
+    requires spring.data.jpa;
+    requires spring.web;
+    requires spring.tx;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.websocket;
+    requires spring.messaging;
+
+    //logging
+    requires slf4j.api;
+
+    //mapper
+    requires modelmapper;
+}
